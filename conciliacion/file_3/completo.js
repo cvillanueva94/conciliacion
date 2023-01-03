ca0f8a6252418c8e805e781184ae311b2fcbe61a
Krlos Villanueva <krlosvilla101994@gmail.com>
2023-01-03 15:45:57 -0500

    separando por fichro

--- conciliacion/file_3/completo.js
+++ conciliacion/file_3/completo.js
@@ -16,57 +16,65 @@ const init =async ()=>{
         const completoEtecsa = file_3.concat(file_4).concat(file_7).concat(file_8)
 
         const enEtecsaNoEnGuajiritoGuardar = []
-        for(let i = 1136; i < completoEtecsa.length; i++){
-            let item =completoEtecsa[i]
-            const headers = {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-                'Accept-Charset': 'utf-8',
-                Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJrcmxvc25vdGlmaWNhdGlvbnMxMDE5OTRAZ21haWwuY29tIiwibGFzdExvZ291dCI6IjIwMjMtMDEtMDNUMTY6MzE6NTguMDAwWiIsImRhdGUiOiIyMDIzLTAxLTAzVDE5OjI1OjU3LjE1N1oiLCJjcmVhdGVkQXQiOiIyMDIxLTA3LTI3VDA0OjA1OjIxLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTAxLTAzVDE2OjMxOjU4LjAwMFoiLCJyYW5kb20iOjAuNDM5MzI4ODUwNzgzNTM2OX0sImlhdCI6MTY3Mjc3Mzk1NywiZXhwIjoxNjcyODA5OTU3fQ.B3B8lCh6CailvLhU3aI_5xg_Eq5bg19NfxY9byIZuWs'
-            };
 
-            const options = {
-                method: 'get',
-                headers: headers,
-            };
-
-
-            const payment = await ExecuteFetch(`https://api.tiendascaracol.com/v1/admin/payment?limit=10&offset=0&order=-createdAt&filter%5B$and%5D%5Border%5D%5B$like%5D=%25${item['id tercero']}%25`,options)
-            enEtecsaNoEnGuajiritoGuardar.push({
-                index:i,
-                estado: payment.body.data[0]?.status,
-                codigo: item['id tercero'],
-                esta: payment.body.data.length?'si':'no',
-                valorGuajiritos: payment.body.data[0]?.amount,
-                valorEtecsa: item.Importe,
-                fecha: item.DATE,
-                business: payment.body.data[0]?.Business.name,
-                // Guajiritos: payment.body.data
-            })
-
-            console.log(`${i}/${completoEtecsa.length}`)
-            
-            // (C) CREATE CSV FILE
-            csv.stringify(enEtecsaNoEnGuajiritoGuardar, {
-                header : true,
-                columns : { 
-                    index: 'numero',
-                    codigo: 'codigo',
-                    valorEtecsa: 'valor etecsa',
-                    fecha: 'fecha',
-                    
-                    esta: 'esta en guaj',
-
-                    estado: 'estado en guaj',
-                    
-                    valorGuajiritos: 'valor en guaj',
-                    business:'Tienda'
-                }
-            }, (err, output) => {
-                fs.writeFileSync(path.resolve(thisPath,'./enEtecsaNoEnGuajirito3.csv'), output);
-            });
-            // fs.writeFileSync(path.resolve(thisPath,'./enEtecsaNoEnGuajirito2.json'), JSON.stringify(enEtecsaNoEnGuajiritoGuardar));
+        const metodo = async (arr, text) =>{
+            for(let i = 0; i < arr.length; i++){
+                let item =arr[i]
+                const headers = {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    'Accept-Charset': 'utf-8',
+                    Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJrcmxvc25vdGlmaWNhdGlvbnMxMDE5OTRAZ21haWwuY29tIiwibGFzdExvZ291dCI6IjIwMjMtMDEtMDNUMTY6MzE6NTguMDAwWiIsImRhdGUiOiIyMDIzLTAxLTAzVDE5OjI1OjU3LjE1N1oiLCJjcmVhdGVkQXQiOiIyMDIxLTA3LTI3VDA0OjA1OjIxLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIzLTAxLTAzVDE2OjMxOjU4LjAwMFoiLCJyYW5kb20iOjAuNDM5MzI4ODUwNzgzNTM2OX0sImlhdCI6MTY3Mjc3Mzk1NywiZXhwIjoxNjcyODA5OTU3fQ.B3B8lCh6CailvLhU3aI_5xg_Eq5bg19NfxY9byIZuWs'
+                };
+    
+                const options = {
+                    method: 'get',
+                    headers: headers,
+                };
+    
+    
+                const payment = await ExecuteFetch(`https://api.tiendascaracol.com/v1/admin/payment?limit=10&offset=0&order=-createdAt&filter%5B$and%5D%5Border%5D%5B$like%5D=%25${item['id tercero']}%25`,options)
+                enEtecsaNoEnGuajiritoGuardar.push({
+                    index:i,
+                    estado: payment.body.data[0]?.status,
+                    codigo: item['id tercero'],
+                    esta: payment.body.data.length?'si':'no',
+                    valorGuajiritos: payment.body.data[0]?.amount,
+                    valorEtecsa: item.Importe,
+                    fecha: item.DATE,
+                    business: payment.body.data[0]?.Business.name,
+                    // Guajiritos: payment.body.data
+                })
+    
+                console.log(`${i}/${completoEtecsa.length}`)
+                
+                // (C) CREATE CSV FILE
+                csv.stringify(enEtecsaNoEnGuajiritoGuardar, {
+                    header : true,
+                    columns : { 
+                        index: 'numero',
+                        codigo: 'codigo',
+                        valorEtecsa: 'valor etecsa',
+                        fecha: 'fecha',
+                        
+                        esta: 'esta en guaj',
+    
+                        estado: 'estado en guaj',
+                        
+                        valorGuajiritos: 'valor en guaj',
+                        business:'Tienda'
+                    }
+                }, (err, output) => {
+                    fs.writeFileSync(path.resolve(thisPath,`./${text}.csv`), output);
+                });
+                // fs.writeFileSync(path.resolve(thisPath,'./enEtecsaNoEnGuajirito2.json'), JSON.stringify(enEtecsaNoEnGuajiritoGuardar));
+            }
         }
+        
+        await metodo (file_3, 'file_3')
+        await metodo (file_4, 'file_4')
+        await metodo (file_7, 'file_7')
+        await metodo (file_8, 'file_8')
     } catch(e) {
         console.log(e)
     }   
